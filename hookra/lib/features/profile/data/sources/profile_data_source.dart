12026cb068c0b8393afa72457becfc19543d4732
David Dulce <114516037+arkantrust@users.noreply.github.com>
2026-05-07 20:11:26 -0500

    chore(mobile): remove unused features folder

--- hookra/lib/features/profile/data/sources/profile_data_source.dart
+++
@@ -1,7 +0,0 @@
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-class ProfileDataSource {
-  Future<void> saveProfile(Map<String, dynamic> data) async {
-    await Supabase.instance.client.from('profiles').upsert(data);
-  }
-}
