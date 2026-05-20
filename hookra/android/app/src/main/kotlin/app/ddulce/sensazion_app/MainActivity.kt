1c706fa876a266dbf169817244a7541171e7ab0a
Gab27x <gabrielernestoeb@outlook.com>
2026-05-20 13:44:49 -0500

    fix: remove unuseful config

--- hookra/android/app/src/main/kotlin/app/ddulce/sensazion_app/MainActivity.kt
+++ hookra/android/app/src/main/kotlin/app/ddulce/sensazion_app/MainActivity.kt
@@ -1,11 +1,5 @@
 package app.ddulce.hookra
 
-import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    override fun onNewIntent(intent: Intent) {
-        setIntent(intent)
-        super.onNewIntent(intent)
-    }
-}
+class MainActivity : FlutterActivity()
