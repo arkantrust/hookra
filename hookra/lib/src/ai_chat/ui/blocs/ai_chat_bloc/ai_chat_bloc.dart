65354d251fc35e9782b8a2bb4167783ddb266a9f
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-06-06 15:16:28 -0500

    refactor(ai_chat): BLoC uses orgId/teamId instead of content params

--- hookra/lib/src/ai_chat/ui/blocs/ai_chat_bloc/ai_chat_bloc.dart
+++ hookra/lib/src/ai_chat/ui/blocs/ai_chat_bloc/ai_chat_bloc.dart
@@ -10,29 +10,23 @@ part 'ai_chat_state.dart';
 class AiChatBloc extends Bloc<AiChatEvent, AiChatState> {
   AiChatBloc({
     required this._sendMessage,
-    required this._contentId,
-    required this._platform,
-    required this._format,
-    required this._title,
+    required this._orgId,
+    required this._teamId,
   }) : super(const AiChatInitial()) {
     on<AiChatStarted>(_onStarted);
   }
 
   final SendMessageUseCase _sendMessage;
-  final String _contentId;
-  final String _platform;
-  final String _format;
-  final String _title;
+  final String _orgId;
+  final String _teamId;
 
   void _onStarted(AiChatStarted event, Emitter<AiChatState> emit) {
     emit(
       AiChatReady(
         provider: HookraChatProvider(
           sendMessage: _sendMessage,
-          contentId: _contentId,
-          platform: _platform,
-          format: _format,
-          title: _title,
+          orgId: _orgId,
+          teamId: _teamId,
         ),
       ),
     );
